32fcff1cf801e724ca97be2b47c51d92072967d2
Oliver Song <80020297+obs4qqf@users.noreply.github.com>
2021-07-20 17:01:00 -0400

    Test connection between front and back end

--- appbackend/app.js
+++ appbackend/app.js
@@ -4,6 +4,10 @@ const app = express();
 
 app.get('/', (req, res) => res.send('Hello World'))
 
+app.get('/api', (req, res) => {
+    res.json({ message : "Hello World From Server!"})
+})
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
