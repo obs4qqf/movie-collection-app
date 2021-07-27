c427b5c4361cf63e61edbdbd897178c79e6e9f12
Oliver Song <80020297+obs4qqf@users.noreply.github.com>
2021-07-27 13:03:06 -0400

    Began React call to Express to get movie information

--- appbackend/app.js
+++ appbackend/app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const axios = require("axios")
 const app = express();
 
 // app.get('/', (req, res) => res.send('Hello World'))
@@ -7,6 +7,13 @@ const app = express();
 // app.get('/api', (req, res) => {
 //     res.json({ message : "Hello World From Server!"})
 // })
+
+app.get('/movie', async (req, res) => {
+    const title = req.query.title
+    axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_KEY}&query=${title}`)
+       .then(data => res.json(data))
+       .catch(err => console.log(err));
+})
 
 const PORT = process.env.PORT || 5000;
 
--- appfrontend/src/App.js
+++ appfrontend/src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import SearchMovie from './components/SearchMovie'
 import Movies from './components/Movies'
+import logo from './TMDbLogo.svg'
 
 function App() {
   const [movieData, setMovieData] = useState([
@@ -24,14 +25,18 @@ function App() {
     }
   ])
 
-  const addMovie = (movie) => {
-    setMovieData([...movieData, {id: 4, title: movie, genre: 'Genre', description: 'Desc'}])
+  const addMovie = async (movie) => {
+    const res = await fetch(`http://localhost:5000/movie?title=${movie}`)
+    const data = await res.json()
+    console.log(data)
   }
 
   return (
     <div className="app">
       <SearchMovie addMovie={addMovie} />
       <Movies movieData={movieData} />
+      <h4>This product uses the TMDb API but is not endorsed or certified by TMDb.</h4>
+      <img src={logo} alt="TMDb Logo"/>
     </div>
   );
 }
