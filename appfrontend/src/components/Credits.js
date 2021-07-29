d4c33e34b4341c020a0171e1649087a25cffcd60
Oliver Song <80020297+obs4qqf@users.noreply.github.com>
2021-07-29 13:03:36 -0400

    Update styling of header, footer, and movie cards

--- appfrontend/src/components/Credits.js
+++ appfrontend/src/components/Credits.js
@@ -2,10 +2,10 @@ import logo from '../TMDbLogo.svg'
 
 const Credits = () => {
     return (
-        <div>
+        <footer>
             <h4>This product uses the TMDb API but is not endorsed or certified by TMDb.</h4>
-            <img src={logo} alt="TMDb Logo"/>
-        </div>
+            <img src={logo} alt="TMDb Logo" width="300"/>
+        </footer>
     )
 }
 
--- appfrontend/src/components/SearchMovie.js
+++ appfrontend/src/components/SearchMovie.js
@@ -4,11 +4,13 @@ const SearchMovie = ({addMovie}) => {
     const [movieName, setMovieName] = useState('')
 
     return (
-        <div>
-            <h4>Search a movie below:</h4>
-            <input type='text' onChange={(e) => setMovieName(e.target.value)} placeholder='Movie name'/>
-            <button onClick={() => addMovie(movieName)}>Search Movie</button>
-        </div>
+        <header>
+            <h1>Search a movie below:</h1>
+            <div id='search-form'>
+                <input type='text' onChange={(e) => setMovieName(e.target.value)} placeholder='Movie name'/>
+                <button onClick={() => addMovie(movieName)}>Search Movie</button>
+            </div>
+        </header>
     )
 }
 
